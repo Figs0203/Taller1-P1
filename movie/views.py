26d9f9fec353d5c80e0f86b136919446e713da3c
Figs0203 <figuesicsi@gmail.com>
2025-07-31 11:43:23 -0500

    Aplicacion sencilla terminada y base de datos funcional

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
     #return HttpResponse("<h1>Welcome to Home Page</h1>")
-    return render(request, 'home.html', {'name':'Agustín Figueroa'})
+    #return render(request, 'home.html', {'name':'Agustín Figueroa'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'movies': movies, 'searchTerm': searchTerm})   
 
 def about(request):
     return render(request, 'about.html')
